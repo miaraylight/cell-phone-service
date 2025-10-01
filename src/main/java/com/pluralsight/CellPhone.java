c02abf89c82bc50d2f9faaae2150dacf754a7e6c
miaraylight <miaraylight@gmail.com>
2025-10-01 15:46:05 -0500

    add CellPhone class

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,40 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private String serialNumber;
+    private String model;
+    private String carrier;
+    private String owner;
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
